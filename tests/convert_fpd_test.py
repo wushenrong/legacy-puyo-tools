a578833e13e047062ded5cbb06b213f5531c5a2c
Samuel Wu <twopizza9621536@gmail.com>
2025-07-17 22:48:37 -0400

    test: 🧪 Cover create and convert fpd cli command
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- tests/convert_fpd_test.py
+++ tests/convert_fpd_test.py
@@ -17,11 +17,28 @@ def test_convert_fpd() -> None:
         with Path("sample_data.fpd").open("wb") as f:
             f.write(FPD_SAMPLE_STRING)
 
-        result = runner.invoke(
-            convert_fpd, ["sample_data.fpd", "--output", "sample_data.txt"]
-        )
+        result = runner.invoke(convert_fpd, ["sample_data.fpd"])
 
         with Path("sample_data.txt").open("r", encoding=ENCODING) as f:
+            assert f.read(1) == BOM_UTF16_LE.decode(ENCODING)
+            assert f.read() == UNICODE_SAMPLE_STRING
+
+        assert result.exit_code == 0
+
+
+def test_convert_fpd_with_output() -> None:
+    """Test converting a fpd file."""
+    runner = CliRunner()
+
+    with runner.isolated_filesystem():
+        with Path("sample_data.fpd").open("wb") as f:
+            f.write(FPD_SAMPLE_STRING)
+
+        result = runner.invoke(
+            convert_fpd, ["sample_data.fpd", "--output", "sample.txt"]
+        )
+
+        with Path("sample.txt").open("r", encoding=ENCODING) as f:
             assert f.read(1) == BOM_UTF16_LE.decode(ENCODING)
             assert f.read() == UNICODE_SAMPLE_STRING
 
--- tests/create_fpd_test.py
+++ tests/create_fpd_test.py
@@ -27,6 +27,25 @@ def test_create_fpd() -> None:
         assert result.exit_code == 0
 
 
+def test_create_fpd_with_output() -> None:
+    """Test creating a fpd file."""
+    runner = CliRunner()
+
+    with runner.isolated_filesystem():
+        with Path("sample_data.txt").open("w", encoding=ENCODING) as f:
+            f.write(BOM_UTF16_LE.decode(ENCODING))
+            f.write(UNICODE_SAMPLE_STRING)
+
+        result = runner.invoke(
+            create_fpd, ["sample_data.txt", "--output", "sample.fpd"]
+        )
+
+        with Path("sample.fpd").open("rb") as f:
+            assert f.read() == FPD_SAMPLE_STRING
+
+        assert result.exit_code == 0
+
+
 def test_create_fpd_no_bom() -> None:
     """Test creating an FPD file without a Byte Order Mark."""
     runner = CliRunner()
