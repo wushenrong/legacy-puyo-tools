30661d378e45c93a48034b6937bd8de0f2ac3a5f
Samuel Wu <twopizza9621536@gmail.com>
2025-07-23 18:21:28 -0400

    Change Python version and license expression (#24)
    * docs: 📝 Update license expression for ChatGPT generated code.
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>
    
    * BREAKING CHANGE: 🚨 Change Python version and version scheme
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>
    
    ---------
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -8,17 +8,13 @@ from legacy_puyo_tools.fpd import Fpd
 from tests.conftest import SAMPLE_FPD_STRING
 
 
-def test_get_fpd_character() -> None:
-    """Test getting a character from a fpd character table."""
+def test_misc_fpd() -> None:
+    """Test getting a character and index from a fpd character table."""
     fpd_data = Fpd.decode(SAMPLE_FPD_STRING)
+    lookup_table = fpd_data.create_lookup_table()
 
     # The 6th index in the sample fpd data should be "3"
     assert fpd_data[5] == "3"
-
-
-def test_lookup_fpd_character() -> None:
-    """Test looking up the index of a fpd character."""
-    lookup_table = Fpd.decode(SAMPLE_FPD_STRING).create_lookup_table()
 
     # The character 佛 in the sample fpd data should be the 9th index
     assert lookup_table["佛"] == 9
